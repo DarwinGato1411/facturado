7ec8a83056a19be1c256a77b0dd6940857ddf781
Darwin <darwinvinicio14_11@hotmail.com>
2022-12-12 16:03:23 -0500

    Cambio decimales factura

--- FacturadorFacturado/FacturadorFacturado/build/generated-sources/ap-source-output/com/ec/entidad/DetalleNotaDebitoCredito_.java
+++ FacturadorFacturado/FacturadorFacturado/build/generated-sources/ap-source-output/com/ec/entidad/DetalleNotaDebitoCredito_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-12-06T10:37:11")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-12-12T15:49:36")
 @StaticMetamodel(DetalleNotaDebitoCredito.class)
 public class DetalleNotaDebitoCredito_ { 
 
--- FacturadorFacturado/FacturadorFacturado/build/generated-sources/ap-source-output/com/ec/entidad/ProductoProveedorPK_.java
+++ FacturadorFacturado/FacturadorFacturado/build/generated-sources/ap-source-output/com/ec/entidad/ProductoProveedorPK_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-12-06T10:37:11")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-12-12T15:49:36")
 @StaticMetamodel(ProductoProveedorPK.class)
 public class ProductoProveedorPK_ { 
 
